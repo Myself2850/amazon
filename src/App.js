2542635c5e81ed7e16ec6a6e2fd672415e177c28
Myself2850 <natsu.dragneel2850@gmail.com>
2022-11-27 11:24:27 +0300

    first app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Header/>
+    <h1>hello world</h1>
+     
+    </div>
+  );
+}
+
+export default App;
